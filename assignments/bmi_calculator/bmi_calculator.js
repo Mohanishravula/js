6abf50c739aa656de49e0d1ccbde75e2a73d9ccf
Mohanishravula <ravulamohanish9064@gmail.com>
2022-06-14 12:56:53 +0530

    PASSESD JS1

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,11 +1,11 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
-    if (typeof mass=== "number" && typeof height=== "number" && mass>0 && height>o){
+    if (typeof mass=== "number" && typeof height=== "number" && mass>0 && height>0){
         return mass/(height*height);
     }
     else {
-        return "INVAILD INPUT";
+        return "INVALID INPUT";
     }
 }
 
